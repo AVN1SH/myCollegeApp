085e65692edba7f14dde51954ce51e7c9a80e3c7
Avnish Kumar Sharma <126967798+AVN1SH@users.noreply.github.com>
2024-04-26 21:32:29 +0530

    added routing setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import Home from "./pages/Home.tsx"
+
+import {Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./store/store.ts";
+import Registration from './pages/Registration.tsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route path='' element={<Home />} />
+      <Route path="/registration" element={<Registration />} />
+    </Route>
+  )
+)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router = {router} />
+    </Provider>
   </React.StrictMode>,
 )
