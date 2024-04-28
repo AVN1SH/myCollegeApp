2ce9f8d54f3b5d0abddd682fe21a6dec8a5d96c7
Avnish Kumar Sharma <126967798+AVN1SH@users.noreply.github.com>
2024-04-28 12:26:20 +0530

    created zod schema for login

--- src/schema/zod.ts
+++ src/schema/zod.ts
@@ -12,10 +12,15 @@ const registration = z.object({
   regId: z.string().min(15, "Registration Id must be Valid.").max(15, "Registration Id must be Valid."),
   email: z.string().email("Invalid Email Id"),
   mobNum: z.string().min(10, "Mobile Number must contain 10 digits").max(10, "Mobile Number must contain 10 digits"),
-  password: z.string().min(8, "Password must have at least 8 characters").max(20, "password must be under 20 charecters").refine(value => password = value),
+  password: z.string().min(8, "Password must have at least 8 characters").max(20, "Password must be under 20 charecters").refine(value => password = value),
   confirmPassword: z.string().max(20).refine((value) => password === value, {
     message : "Password do not matches."
   }),
 })
 
-export { registration }
+const login = z.object({
+  email : z.string().email("Invalid Email Id"),
+  password : z.string().max(20, "Password must be under 20 charecters")
+})
+
+export { registration, login }
