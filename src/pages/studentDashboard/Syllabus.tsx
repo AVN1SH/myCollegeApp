bf633b709be9a7af846b73d5e2e1e4a22176e102
Avnish Kumar Sharma <126967798+AVN1SH@users.noreply.github.com>
2024-05-03 22:37:30 +0530

    added some changes in  syllabus component to fetch data from store provider

--- src/pages/studentDashboard/Syllabus.tsx
+++ src/pages/studentDashboard/Syllabus.tsx
@@ -1,8 +1,26 @@
+import { useSelector, useDispatch } from "react-redux"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import { AuthState } from "@/features/authSlice"
 
 const Syllabus = () => {
+  const userData = useSelector((state : AuthState) => state.userData);
   return (
     <div>
-      
+      {userData.course.syllabus.map((data : any) => 
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>{data.title}</AccordionTrigger>
+            <AccordionContent>
+              {data.description}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      )}
     </div>
   )
 }
