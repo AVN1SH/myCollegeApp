573116b49637734fb67ef9c98a19728982685bf8
Avnish Kumar Sharma <126967798+AVN1SH@users.noreply.github.com>
2024-05-06 21:18:23 +0530

    initial commit , testing protected route

---
+++ src/components/auth/ProtectedRoute.tsx
@@ -0,0 +1,19 @@
+import { PropsWithChildren, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+type ProtectedRouteProps = PropsWithChildren;
+
+const ProtectedRoute = ({ children } : ProtectedRouteProps) => {
+  const [user, setUser] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(!user) {
+      navigate("/login", {replace : true});
+    }
+  }, [navigate, user])
+
+  return children
+}
+
+export default ProtectedRoute
