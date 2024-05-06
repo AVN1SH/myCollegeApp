e78165c329b6eb72441e9ec3c2e98f977be56c8d
Avnish Kumar Sharma <126967798+AVN1SH@users.noreply.github.com>
2024-05-06 23:30:54 +0530

    initial commit for new timeline component

---
+++ src/components/TimeLine.tsx
@@ -0,0 +1,17 @@
+import times from "@/../public/times.json";
+
+const TimeLine = () => {
+  return (
+    <div className="flex w-full rounded overflow-y-scroll h-[calc(100vh-12rem)] bg-white p-3 custom-scroll-bar">
+      <div className="flex flex-col flex-1 justify-between h-[4000px] pb-[170px] font-semibold text-slate-400">
+        {times.map((time) => <p key={time}>{time}</p>)}
+      </div>
+      <div className="flex-[10] flex flex-col justify-between h-[4000px] py-3">
+        {times.map((item) => <hr key={item} className="border-slate-300"></hr>)}
+        {times.map((item) => <hr key={item} className="border-slate-300"></hr>)}
+      </div>
+    </div>
+  )
+}
+
+export default TimeLine
