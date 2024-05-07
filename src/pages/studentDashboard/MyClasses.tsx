ff9fae0e42d2ee47d00e6c728bd0caa3125da22e
Avnish Kumar Sharma <126967798+AVN1SH@users.noreply.github.com>
2024-05-07 20:31:30 +0530

    fixed know bugs, and improve ui

--- src/pages/studentDashboard/MyClasses.tsx
+++ src/pages/studentDashboard/MyClasses.tsx
@@ -2,20 +2,44 @@ import TimeLine from "@/components/TimeLine";
 import { buttonVariants } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar"
 import { cn } from "@/lib/utils";
-import { useState } from "react"
+import { faCalendar, faCalendarTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react"
 
 const MyClasses = () => {
   const [date, setDate] = useState<Date | undefined>(new Date());
+  const [formattedDate, setFormattedDate] = useState('');
+  const [formattedWeek, setFormattedWeek] = useState(''); 
+
+  useEffect(() => {
+    const options1: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+
+    const options2 : Intl.DateTimeFormatOptions = {
+      weekday: "long",
+    }
+  
+    const formatDate = date?.toLocaleDateString("en-US", options1);
+    const formatWeek = date?.toLocaleDateString("en-US", options2);
+
+    if(formatDate) setFormattedDate(formatDate);
+    if(formatWeek) setFormattedWeek(formatWeek);
+  }, [date])
 
   return (
     <div className="relative m-1 w-[clac(100%-280px)]">
       <div className="absolute w-full bg-orange-500 h-72 rounded">
         <h1 className="font-bold text-3xl pl-3 pt-3 text-white">
-          Overview
+          My Classes | Classes and Events Schedules
         </h1>
       </div>
-      <div className="absolute border-slate-400 border-[1px] border-solid flex bg-gray-100 w-[calc(100%-100px)] top-24 left-1/2 -translate-x-1/2 min-h-[calc(100vh-11rem)] rounded p-3">
-        <div className="flex bg-gray-100 justify-between w-full gap-3">
+      <div className="absolute border-slate-400 border-[1px] border-solid flex flex-col bg-gray-100 w-[calc(100%-100px)] top-24 left-1/2 -translate-x-1/2 min-h-[calc(100vh-11rem)] rounded p-3">
+        <div className="font-semibold text-2xl text-slate-700 bg-white mb-1 rounded p-2 pl-2"><FontAwesomeIcon icon={faCalendar}/> {formattedDate}<span className="divider-vertical border-solid border-[1px] border-orange-300 mx-2"></span><span className="font-thin text-md">{formattedWeek}</span>
+        </div>
+        <div className="flex bg-gray-100 justify-between w-full gap-1">
           <div className="flex-[1.7] bg-white rounded flex flex-row">
             <TimeLine />
           </div>
@@ -30,7 +54,7 @@ const MyClasses = () => {
                 row: "flex w-full",
                 cell: "h-14 w-14 text-center text-sm relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-orange-100 [&:has([aria-selected])]:bg-orange-100 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20 w-full rounded-full hover:bg-orange-100 flex items-center justify-center hover:cursor-pointer",
                 day_selected:"bg-orange-500 text-orange-500-foreground hover:bg-orange-600 hover:text-orange-600-foreground focus:bg-orange-500 focus:text-orange-500-foreground text-white",
-                day: "w-8 h-8 rounded-full",
+                day: "w-12 h-12 rounded-full",
                 day_today: "bg-accent text-accent-foreground text-orange-500 font-semibold",
               }}
             />
