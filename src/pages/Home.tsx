480f206c5bb1a2cf3c3af340c80d16f93ee48b26
Avnish Kumar Sharma <126967798+AVN1SH@users.noreply.github.com>
2024-05-29 20:09:08 +0530

    added classname in home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import Slider from "@/components/animation/Slider"
 import Footer from "@/components/footer/Footer"
+import BadgeNav from "@/components/navbar/BadgeNav"
 
 const Home = () => {
   return (
-    <div>
+    <div className="pt-16 overflow-x-hidden">
       <Slider />
+      <BadgeNav />
       <Footer />
     </div>
   )
