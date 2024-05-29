e87831e261a0568d4fc41805b641dde87cd30a44
Avnish Kumar Sharma <126967798+AVN1SH@users.noreply.github.com>
2024-05-29 20:00:34 +0530

    conditional footer rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLocation } from "react-router-dom"
 import NavBar from "./components/navbar/NavBar"
+import Footer from "./components/footer/Footer"
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const location = useLocation();
+  const { pathname } = location;
+  const [path, setPath] = useState(location.pathname);
+
+  useEffect(() => {
+    setPath(pathname);
+  }, [pathname]);
+
   return (
     <div className="">
       <NavBar />
       <main>
         <Outlet />
       </main>
+      {!path.includes("dashboard") && <Footer />}
     </div>
   )
 }
