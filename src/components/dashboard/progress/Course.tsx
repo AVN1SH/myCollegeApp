24d263267b94bccd9ea96305657e0d3532173db6
Avnish Kumar Sharma <126967798+AVN1SH@users.noreply.github.com>
2024-06-07 10:24:38 +0530

    course graph initial commit

---
+++ src/components/dashboard/progress/Course.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const data = {
+  labels: ['Coverd', 'Uncoverd'],
+  datasets: [
+    {
+      label: '%',
+      data: [30, 70],
+      backgroundColor: [
+        'rgba(255, 144, 32, 0.8)',
+        'rgba(255, 194, 52, 0.8)'
+      ],
+      borderColor: [
+        "rgba(255, 144, 32, 1)",
+        "rgba(255, 194, 52, 1)"
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const Course = () => {
+  return (
+    <Doughnut data={data} />
+  )
+}
+
+export default Course
