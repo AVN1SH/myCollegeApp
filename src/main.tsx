2fb5a2e7c4836d83d5f6d2945800eb1f27ea0a95
Avnish Kumar Sharma <126967798+AVN1SH@users.noreply.github.com>
2024-06-10 18:54:10 +0530

    removed comma

--- src/main.tsx
+++ src/main.tsx
@@ -69,5 +69,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Provider store={store}>
       <RouterProvider router = {router} />
     </Provider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
