bc35f598b64cf8ae6236f9b5cb5d5e94780fce59
Avnish Kumar Sharma <126967798+AVN1SH@users.noreply.github.com>
2024-06-13 23:00:47 +0530

    created some type for easy use

--- src/utils/navigation.ts
+++ src/utils/navigation.ts
@@ -1,17 +1,17 @@
 import { faBookOpen, faChalkboard, faChalkboardUser, faChartPie, faCheckToSlot, faCreditCard, faFileInvoice, faFileLines, faFolderOpen, faHome, faMessage, faUserTie } from "@fortawesome/free-solid-svg-icons"
 
 export const stdNav = [
-  { name: 'Overview', href: "/student-dashboard/overview", current: true, list: true, icon: faHome, subNav : ''},
-  { name: 'Admissions', href: "/student-dashboard/admission", current: false, list: false, icon:faCheckToSlot, subNav : "/personal-info" },
-  { name: 'Documentations', href: "/student-dashboard/documentation", current: false, list: false, icon:faFileLines, subNav : '' },
-  { name: 'Payment', href: '/student-dashboard/payment', current: false, list: false, icon:faCreditCard, subNav : '' },
-  { name: 'My Classes', href: '/student-dashboard/my-classes', current: false, list: true, icon:faChalkboardUser, subNav : '' },
-  { name: 'Results/Grades', href: '/student-dashboard/result', current: false, list: true, icon:faFileInvoice, subNav : '' },
-  { name: 'Upcoming Assignments', href: '#', current: false, list: true, icon:faFolderOpen, subNav : '' },
-  { name: 'Syllabus', href: '/student-dashboard/syllabus', current: false, list: true, icon:faBookOpen, subNav : '' },
-  { name: 'Progress Report', href: "/student-dashboard/progress-report", current: false, list: true, icon:faChartPie, subNav : '' },
-  { name: 'Teachers', href: '/student-dashboard/teachers', current: false, list: true, icon:faUserTie, subNav : '' },
-  { name: "FeedBack", href: '/student-dashboard/feedback', current: false, list: true, icon:faMessage, subNav : '' },
+  { name: 'Overview', href: "/student-dashboard/overview", current: true, list: 'both', icon: faHome, subNav : ''},
+  { name: 'Admissions', href: "/student-dashboard/admission", current: false, list: '1', icon:faCheckToSlot, subNav : "/personal-info" },
+  { name: 'Documentations', href: "/student-dashboard/documentation", current: false, list: '1', icon:faFileLines, subNav : '' },
+  { name: 'Payment', href: '/student-dashboard/payment', current: false, list: '1', icon:faCreditCard, subNav : '' },
+  { name: 'My Classes', href: '/student-dashboard/my-classes', current: false, list: '2', icon:faChalkboardUser, subNav : '' },
+  { name: 'Results/Grades', href: '/student-dashboard/result', current: false, list: '2', icon:faFileInvoice, subNav : '' },
+  { name: 'Upcoming Assignments', href: '#', current: false, list: '2', icon:faFolderOpen, subNav : '' },
+  { name: 'Syllabus', href: '/student-dashboard/syllabus', current: false, list: '2', icon:faBookOpen, subNav : '' },
+  { name: 'Progress Report', href: "/student-dashboard/progress-report", current: '2', list: true, icon:faChartPie, subNav : '' },
+  { name: 'Teachers', href: '/student-dashboard/teachers', current: false, list: '2', icon:faUserTie, subNav : '' },
+  { name: "FeedBack", href: '/student-dashboard/feedback', current: false, list: 'both', icon:faMessage, subNav : '' },
 ]
 export const facultyNav = [
   { name: 'Overview', href: "/faculty-dashboard/overview", current: true, list: true, icon: faHome, subNav : ''},
---
+++ src/utils/userDataType.ts
@@ -0,0 +1,35 @@
+interface UserData {
+  id : string;
+  registration_id: string
+  first_name: string,
+  middle_name: string | null;
+  last_name: string | null;
+  role: string,
+  phone: string,
+  email: string,
+  father_name: string,
+  mother_name: string,
+  sex: string
+  cast: string
+  dob: string
+  nationality: string
+  building_number: string;
+  locality: string,
+  sublocality: string,
+  state: string
+  district: string
+  pincode: string
+  contract_number: string
+  alternate_number: string
+  status: string
+  year: string
+  school: string
+  roll_code: string
+  total_marks: string
+  obtain_marks: string
+  photo: string
+  signature: string
+  adhar: string
+  tenth: string
+  twelth: string
+}
